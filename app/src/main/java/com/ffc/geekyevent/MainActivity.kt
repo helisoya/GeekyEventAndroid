27745eaf0609c77af1a3d07ad6091c86331d5376
titou <titouanmagnin2@gmail.com>
2023-03-01 18:47:00 +0100

    drawer

--- app/src/main/java/com/ffc/geekyevent/MainActivity.kt
+++ app/src/main/java/com/ffc/geekyevent/MainActivity.kt
@@ -2,23 +2,32 @@ package com.ffc.geekyevent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.customview.widget.Openable
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this,navController)//bouton retour en haut
+        val navView:NavigationView = findViewById(R.id.navView);
+        drawerLayout = findViewById(R.id.drawerLayout)
 
+        NavigationUI.setupWithNavController(navView, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout) //bouton retour en haut + drawer
 
     }
 
-    //bouton retour (en haut a gauche)
+    //bouton retour (en haut a gauche) et bouton ||| menu
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController,drawerLayout)
     }
 }
